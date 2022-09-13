6869da7f6f046b652df2551d1d41c5497a5315ec
Caleb Brewer <97901509+CBrew913@users.noreply.github.com>
2022-09-12 19:56:38 -0500

    Adding filter functionalitiy.

--- static/js/app.js
+++ static/js/app.js
@@ -4,15 +4,34 @@ const tableData = data;
 // referencing the HTML table using d3
 var tbody = d3.select("tbody");
 
-// defining the function that will build the table
+// defining the function to build the table
 function buildTable(data) {
     tbody.html("");
+    
     data.forEach((dataRow) => {
         let row = tbody.append("tr");
+        
         Object.values(dataRow).forEach((val) => {
             let cell = row.append("td");
             cell.text(val);
             }
         );
     });
-}
+};
+
+// defining the function to handle a user clicking a button
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    buildTable(filteredData);
+};
+
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+
+buildTable(tableData);
